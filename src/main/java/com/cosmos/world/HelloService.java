6a64e9741140a7d4ee72a43f70d1114f26a1dd79
Sachin Kale <kalesachin@gmail.com>
2019-03-21 22:23:50 +0530

    HelloWorld project to get started with Wild-Fly on Open-Shift

---
+++ src/main/java/com/cosmos/world/HelloService.java
@@ -0,0 +1,15 @@
+package com.cosmos.world;
+
+/**
+ * 
+ *
+ * @author Sachin Kale
+ *
+ */
+public class HelloService {
+
+    String createHelloMessage(String name) {
+        return "Hello Open Shift..." + name + "!";
+    }
+
+}
